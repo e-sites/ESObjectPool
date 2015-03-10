20b92fae82c39f5580c68b29ead3ee92d8c5e2fc
Bas van Kuijck <bas@e-sites.nl>
2015-03-10 14:24:27 +0100

    Xcodeproject

---
+++ ESObjectPoolTests/ESObjectPoolTests.m
@@ -0,0 +1,27 @@
+//
+//  ESObjectPoolTests.m
+//  ESObjectPoolTests
+//
+//  Created by Bas van Kuijck on 10-03-15.
+//  Copyright (c) 2015 e-sites. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ESObjectPoolTests : XCTestCase
+
+@end
+
+@implementation ESObjectPoolTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+@end
